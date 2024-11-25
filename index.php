b50a9f946f24edc9d3b7cfc165a252daf5d842a6
aditGani <adtgni@gmail.com>
2024-11-25 09:13:16 +0800

    versi awal

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$a = 2695886;
+$b = substr($a,0,4). ".". substr($a,4,3);
+$lot = 0.5;
+$i = $a;
+$hit = 0;
+while($i<2708786){
+    if($lot > 0.9){
+        $hit = substr($lot, 0)."00";
+    } else if($lot > 0.09){
+        $hit = substr($lot, 2)."0";
+    } else {
+        $hit = substr($lot, 2);
+    }
+    $b = substr($a,0,4). ".". substr($a,4,3);
+    $c = substr($i,0,4). ".". substr($i,4,3);
+    echo $b." - ".$c.": ". number_format(15875 * ($c - $b) * $hit)."<br>";
+    $i++;
+}
+?>
